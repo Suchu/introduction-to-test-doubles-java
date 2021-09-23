36a963019cb06dd3b1cf4415a0325615f7319101
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-24 00:03:36 +0100

    Explored parameterized test case

--- src/test/java/BubbleTeaOrderServiceTest.java
+++ src/test/java/BubbleTeaOrderServiceTest.java
@@ -2,28 +2,57 @@ import com.techreturners.bubbleteaordersystem.model.*;
 import com.techreturners.bubbleteaordersystem.service.BubbleTeaMessenger;
 import com.techreturners.bubbleteaordersystem.service.BubbleTeaOrderService;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import testhelper.DummySimpleLogger;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+@RunWith(Parameterized.class)
 public class BubbleTeaOrderServiceTest {
 
-    private DebitCard testDebitCard;
     private PaymentDetails paymentDetails;
-    private DummySimpleLogger dummySimpleLogger;
     private BubbleTeaMessenger mockMessenger;
     private BubbleTeaOrderService bubbleTeaOrderService;
+    private final String name;
+    private final String address;
+    private final String debitCard;
+    private final BubbleTeaTypeEnum bubbleTeaType;
+
+    public BubbleTeaOrderServiceTest(String name,
+                                     String address,
+                                     String debitCard,
+                                     BubbleTeaTypeEnum bubbleTeaType) {
+        this.name = name;
+        this.address = address;
+        this.debitCard = debitCard;
+        this.bubbleTeaType = bubbleTeaType;
+    }
 
     @BeforeEach
     public void setup() {
-        testDebitCard = new DebitCard("0123456789");
-        paymentDetails = new PaymentDetails("hello kitty", "sanrio puroland", testDebitCard);
-        dummySimpleLogger = new DummySimpleLogger();
+        DebitCard testDebitCard = new DebitCard(debitCard);
+        paymentDetails = new PaymentDetails(name, address, testDebitCard);
+        DummySimpleLogger dummySimpleLogger = new DummySimpleLogger();
         mockMessenger = mock(BubbleTeaMessenger.class);
         bubbleTeaOrderService = new BubbleTeaOrderService(dummySimpleLogger, mockMessenger);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"hello kitty", "sanrio puroland", "0123456789", BubbleTeaTypeEnum.MatchaMilkTea},
+                {"hello kitty1", "sanrio puroland1", "0173456786", BubbleTeaTypeEnum.PeachIceTea},
+                {"hello kitty2", "sanrio puroland2", "0183456784", BubbleTeaTypeEnum.LycheeIceTea},
+                {"hello kitty3", "sanrio puroland3", "0133456781", BubbleTeaTypeEnum.JasmineMilkTea},
+                {"hello kitty4", "sanrio puroland4", "0143456785", BubbleTeaTypeEnum.OolongMilkTea},
+        });
     }
 
     @Test
@@ -34,14 +63,15 @@ public class BubbleTeaOrderServiceTest {
         BubbleTeaRequest bubbleTeaRequest = new BubbleTeaRequest(paymentDetails, bubbleTea);
 
         BubbleTeaOrderRequest expectedResult = new BubbleTeaOrderRequest(
-                "hello kitty",
-                "sanrio puroland",
-                "0123456789",
-                BubbleTeaTypeEnum.OolongMilkTea
+                name,
+                address,
+                debitCard,
+                bubbleTeaType
         );
 
         //Act
         BubbleTeaOrderRequest result = bubbleTeaOrderService.createOrderRequest(bubbleTeaRequest);
+//        System.out.println(result);
 
         //Assert
         assertEquals(expectedResult.getName(), result.getName());
